0b9278d3c371884ca3a2ce93b1a185f61c1a70b0
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-07 01:59:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,5 +129,5 @@ def adding_to_dennis
         }
      }
      new_language = "Assembly"
-     programmer_hash[:dennis_ritchie][:languages][]
+     programmer_hash[:dennis_ritchie][:languages] + new_language
 end
